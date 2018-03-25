5dabcdef7e59543dfc276cacf35289f958da34cf
Wafflebasket <luke.keijer@mail.com>
2018-03-25 18:08:54 +0200

    Add project files.

---
+++ ConsoleTestGame2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTestGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
